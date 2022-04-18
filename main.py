2c734f9ddd535e9445be598577b5fc51c9a75a26
SanketJadhav7d3 <lonewolf7d3@gmail.com>
2022-04-18 22:18:37 +0530

    New state

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/Users/sanketjadhav/opt/miniconda3/envs/QuantumProgramming/bin/python
+
 # game of life project
 # Rules
 # Any live cell with fewer than two live neighbours dies, as if by
@@ -6,7 +8,7 @@
 # dies, as if by overpopulation. Any dead cell with exactly three live
 # neighbours becomes a live cell by reproduction.
 
-from this import d
+import math
 import pygame
 import numpy as np
 from pygame.locals import *
@@ -14,54 +16,56 @@ from pygame.locals import *
 SCREEN_HEIGHT = 480
 SCREEN_WIDTH = 480
 
-cells = np.random.randint(2, size=(80, 80))
-
 cell_size = 5
 
-def get_info(i, j):
-    global cells 
+rows = SCREEN_HEIGHT // cell_size
+cols = SCREEN_WIDTH // cell_size
+
+
+def get_info(i, j, cells):
     info = 0
     for a in range(i-1, i+2):
         for b in range(j-1, j+2):
-            if a == i and b == j: continue 
+            if a == i and b == j: continue
             if a < 0 or b < 0: continue
             if a >= cells.shape[0] or b >= cells.shape[1]: continue
             if cells[a][b] == None: continue
             info += cells[a][b]
-    return info 
+    return info
 
 def update(cells):
     '''
         To update the cells lives
     '''
+    next_gen = np.zeros((rows, cols))
     for i in range(cells.shape[0]):
         for j in range(cells.shape[1]):
             # underpopulation
-            if cells[i][j] == 1 and get_info(i, j) < 2:
-                cells[i][j] = 0
+            if cells[i][j] == 1 and get_info(i, j, cells) < 2:
+                next_gen[i][j] = 0
             # overpopulation
-            if cells[i][j] == 1 and get_info(i, j) > 3:
-                cells[i][j] = 0
+            if cells[i][j] == 1 and get_info(i, j, cells) > 3:
+                next_gen[i][j] = 0
             # reproduction
-            if cells[i][j] == 0 and (get_info(i, j) == 2 or get_info(i, j) == 3):
-                cells[i][j] = 1
+            if cells[i][j] == 0 and (get_info(i, j, cells) == 2 or get_info(i, j, cells) == 3): 
+                next_gen[i][j] = 1
+    return next_gen
 
 
-
-def draw(window, cells):
+def draw(window, cells_):
     '''
         To draw the new generation to the screen
     '''
-    for i in range(cells.shape[0]):
-        for j in range(cells.shape[1]):
+    for i in range(cells_.shape[0]):
+        for j in range(cells_.shape[1]):
             rectangle = pygame.Rect(i * (cell_size + 1), j * (cell_size + 1), cell_size, cell_size)
-            if cells[i][j] == 1:
+            if cells_[i][j] == 1:
                 pygame.draw.rect(window, (31, 81, 255), rectangle)
             else:
                 pygame.draw.rect(window, (0, 0, 0), rectangle)
 
 def main():
-    drawing = False
+    cells = np.random.randint(2, size=(rows, cols))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     while running:
@@ -73,23 +77,9 @@ def main():
                 if event.key == K_ESCAPE:
                     running = False 
 
-            if event.type == pygame.MOUSEMOTION:
-                cur_pos = pygame.mouse.get_pos()
-                if drawing:
-                    if cur_pos[0] % 5 == 0 and cur_pos[1] % 5 == 0:
-                        rectangle = pygame.Rect(cur_pos[0], cur_pos[1], cell_size, cell_size)
-                        pygame.draw.rect(screen, (255, 255, 255), rectangle)
-                        print(cur_pos)
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                drawing = True
-
-            if event.type == pygame.MOUSEBUTTONUP:
-                drawing = False
-
         draw(screen, cells)
 
-        update(cells)
+        cells = update(cells)
 
         pygame.display.update()
 
